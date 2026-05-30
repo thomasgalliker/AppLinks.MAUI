d71b509f9ce9cab7ab561afdb420681ef0a7d8c1
Thomas Galliker <thomas@bluewin.ch>
2026-05-30 20:16:35 +0200

    Use CreateWindow instead of MainPage

--- Samples/AppLinksDemoApp/App.xaml.cs
+++ Samples/AppLinksDemoApp/App.xaml.cs
@@ -5,17 +5,23 @@ namespace AppLinksDemoApp
 {
     public partial class App : Application
     {
+        private readonly IServiceProvider serviceProvider;
+
         public App(IServiceProvider serviceProvider, IAppLinkRuleManager appLinkRuleManager)
         {
+            this.serviceProvider = serviceProvider;
             this.InitializeComponent();
 
             // Register uri processing rules at startup of your app.
             // Here we use static declarations of app link rules.
             // appLinkRuleManager.Add(StaticAppLinkRules.HomeRule);
             // appLinkRuleManager.Add(StaticAppLinkRules.SettingsRule);
+        }
 
-            var mainPage = serviceProvider.GetRequiredService<MainPage>();
-            this.MainPage = new NavigationPage(mainPage);
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var mainPage = this.serviceProvider.GetRequiredService<MainPage>();
+            return new Window(new NavigationPage(mainPage));
         }
     }
 }
