9a6149e102ebcd7be0292b8253dc55cbbb55a812
Thomas Galliker <thomas@bluewin.ch>
2026-05-30 20:17:17 +0200

    Replace FluentAssertions with AwesomeAssertions

--- Tests/AppLinks.MAUI.Tests/GlobalUsings.cs
+++ Tests/AppLinks.MAUI.Tests/GlobalUsings.cs
@@ -1,11 +1,8 @@
-// Global using directives
-
-global using System.Collections.Concurrent;
 global using System.Diagnostics;
 global using AppLinks.MAUI.Services;
 global using AppLinks.MAUI.Tests.Logging;
 global using AppLinks.MAUI.Tests.Utils;
-global using FluentAssertions;
+global using AwesomeAssertions;
 global using Microsoft.Extensions.Logging;
 global using Moq;
 global using Moq.AutoMock;
